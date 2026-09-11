0ce988c753019726f833a4632181123cf0aeb3dd
MilkColors <128tcarney@frhsd.com>
2026-09-11 09:37:52 -0400

    Second Reply

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("RP");
-        System.out.print("Hey RP, I'm AVS");
+        System.out.println("Hey RP, I'm AVS");
+        System.out.println("Hey, RP and AVS! I am TJC");
     }
 
 }
